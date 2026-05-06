d672c6834d950b65e3f3bb25f77d5544b4da73bb
Lord Xenus B. Yacapin <Lordxenusyacapin@gmail.com>
2026-05-06 17:25:27 +0800

    Second commit. Added viewing and searching pokemon feature

--- code.py
+++ code.py
@@ -4,26 +4,46 @@ def menu():
     print("2. View pokemons.")
     print("3. Shop")
     print("4. Exit\n")
-    user_choice = int(input("Go to(1-4): "))
-    return user_choice
+    
+    try:
+        user_choice = int(input("Go to(1-4): "))
+
+        if user_choice == 1:
+            pass
+        elif user_choice == 2:
+            view_pokemons()
+        elif user_choice == 3:
+            pass
+        else:
+            print("Exiting...\n")
+    except:
+        pass
+    finally:
+        print("Thank you for playing pokemon!")
+
+def search(pokemons):
+    pokemon = input("Enter pokemon name: ")
+    if pokemon in pokemons:
+        print("found")
+    else:
+        print("not found")
 
 def view_pokemons():
-    with open("pokemons.txt", "w") as f:
+    print("---[POKEMONS]---\n")
+    with open("pokemons.txt", "r") as f:
         pokemons = f.read()
         print(pokemons)
+    print("1. Search pokemon.")
+    print("2. Back to menu.")
+    print("3. Exit.")
 
-user_choice = menu()
+    user_choice = int(input("Go to(1-3): "))
 
-try:
     if user_choice == 1:
-        pass
+        search(pokemons)
     elif user_choice == 2:
-        view_pokemons()
-    elif user_choice == 3:
-        pass
+        menu()
     else:
-        print("Exiting...\n")
-except:
-    pass
-finally:
-    print("Thank you for playing pokemon!")
+        pass
+
+menu()
